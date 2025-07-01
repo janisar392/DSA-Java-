8aa5248719aa8f5ccdab97790019908509df1933
janisar392 <janisarakhtar392@gmail.com>
2025-07-02 02:47:17 +0530

    graph

---
+++ java-And-dsa-foundation-main/src/DSA/Graph/DFS.java
@@ -0,0 +1,28 @@
+package DSA.Graph;
+
+import java.util.ArrayList;
+
+public class DFS {
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+
+        int V = adj.size();
+        boolean[] visited = new boolean[V+1];
+
+        visited[0] = true;
+        ArrayList<Integer> ans = new ArrayList<>();
+        dfs(0,visited,adj,ans);
+        return ans;
+    }
+
+    public static void dfs(int node, boolean visited[] , ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> list){
+
+        visited[node] = true;
+        list.add(node);
+
+        for(Integer i : adj.get(node)){
+            if(! visited[i]){
+                dfs(i, visited, adj,list);
+            }
+        }
+    }
+}
